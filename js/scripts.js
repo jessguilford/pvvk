3b359b94224bc6a7c60e86a191cb4c8c18676817
jessguilford <jess.guilford@gmail.com>
2019-11-18 14:56:17 -0800

    add line animation to active artist card

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,24 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     }, 1000, 'easeOutSine');
 });
 
+// When the top of the window hits the top of an artist card frame, trigger a line animation
+$(window).scroll(function() {
+  // var topOfWindow = $(window).scrollTop();
+  var el = document.querySelector('#section-gabriel');
+  var pos = el.getBoundingClientRect();
+
+  // pos.top = the distance from the top of the element to the top of the window
+  // pos.bottom = the distance from the bottom of the element to the top of the window
+  // Trigger animation when the top of the element is 50% up the screen
+  if (pos.top < window.innerHeight/2 && pos.bottom >= window.innerHeight*3/4) {
+    $('#section-gabriel').addClass('active');
+  } else if (pos.bottom < 0) {
+    $('#section-gabriel').removeClass('active');
+  } else {
+    $('#section-gabriel').removeClass('active');
+  }
+});
+
 // A handy bit of code for finding the path length of an svg
 // var logo = document.querySelectorAll('#logo path');
 //
